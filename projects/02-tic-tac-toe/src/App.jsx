41156a25d2e7cadaad1a659674fe4f219c86b916
Waldanag <walter@algabits.com>
2024-02-08 19:34:18 -0300

    searching for the winner

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -20,6 +20,17 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
 
@@ -27,9 +38,23 @@ function App() {
 
   const [winner, setWinner] = useState(null)
 
+  const checkWinner = (boardToCheck) => {
+    //Revisamos todas las combinaciones ganadoras para ver quien ganó.
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] && 
+        boardToCheck[a] === boardToCheck[b] && 
+        boardToCheck[a] === boardToCheck[c]) {
+          return boardToCheck[a]
+      }      
+    }
+    return null
+  }
+
   const updateBoard = (index) => {
     //No actualizar posicion si ya tiene algo
-    if (board[index]) return
+    if (board[index] || winner) return
     //Actualizar el tablero
     const newBoard = [...board]
     newBoard[index] = turn
@@ -37,6 +62,11 @@ function App() {
     //Cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    //Revisar si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
   }
 
   return (
