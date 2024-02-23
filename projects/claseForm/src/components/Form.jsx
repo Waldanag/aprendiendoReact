d1677905d9f805a9f547037eaa828a12a414fdaa
Waldanag <walter@algabits.com>
2024-02-23 19:55:15 -0300

    Finishing form

--- projects/claseForm/src/components/Form.jsx
+++ projects/claseForm/src/components/Form.jsx
@@ -6,6 +6,7 @@ const Form = () => {
     const [age, setAge] = useState('')
     const [favoritePokemon, setFavotitePokemon] = useState('')
 
+    console.log(name, age, favoritePokemon);
     const [show, setShow] = useState(false)
     const [err, setErr] = useState(false)
 
@@ -23,33 +24,29 @@ const Form = () => {
     return (
     <div>
         {show ? null : ( 
-        <form onSubmit={handleSubmit}>
-            <label>Nombre Completo: </label>
-            <input 
-            type="text"
-            onChange={(event) =>
-                setName({name: event.target.value })
-            }
-            />
-            <label>Edad: </label>
-            <input
-            type="int"
-            onChange={(event) =>
-                setAge({age: event.target.value })
-            }
-            />
-            <label>Pokemon Favorito: </label>
-            <input
-            type="text"
-            onChange={(event) =>
-                setFavotitePokemon({favoritePokemon: event.target.value })
-            }
-            />
-            <button>Enviar</button>
-        </form>
+            <form onSubmit={handleSubmit}>
+
+                <label>Nombre Completo: </label>
+                <input type="text" onChange={(event) => setName(event.target.value)}/>
+
+                <label>Edad: </label>
+                <input type="number" onChange={(event) => setAge(event.target.value)}/>
+
+                <label>Pokemon Favorito: </label>
+                <input type="text" onChange={(event) => setFavotitePokemon(event.target.value)}/>
+
+                <button>Enviar</button>
+            </form>
         )}
-        {/*usar show para mostrar mensaje de exito de completar el formulario y sacar de pantalla el form*/}
-        {/*usar err para mensajes de error al completar el formulario*/}
+        
+        {show ? 
+            <>
+                <h4>Gracias, {name}!</h4>
+                <h4>Sabemos que {favoritePokemon} es uno de los mejores, gran elección!!</h4>
+            </> 
+            : <p>Coloque sus datos para validar su pokemon</p>}
+    
+        {err ? <p style={{color: 'red'}}>Debe colocar su información correctamente</p> : null}
     </div>
     );
 };
